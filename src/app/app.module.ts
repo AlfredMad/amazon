af7fa03159f3589fd6d309af7f787ba2848d890f
AlfredMad <viaindustrial@yahoo.com>
2021-12-05 11:23:17 -0500

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './assets/navbar/navbar.component';
+import { FooterComponent } from './assets/footer/footer.component';
+import { IndexComponent } from './assets/index/index.component';
+import { ErrorComponent } from './assets/error/error.component';
+// import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { GetComponent } from './modulos/clientes/get/get.component';
+import { CreateComponent } from './modulos/clientes/create/create.component';
+import { EditComponent } from './modulos/clientes/edit/edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    IndexComponent,
+    ErrorComponent,
+    CreateComponent,
+    EditComponent,
+    GetComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
